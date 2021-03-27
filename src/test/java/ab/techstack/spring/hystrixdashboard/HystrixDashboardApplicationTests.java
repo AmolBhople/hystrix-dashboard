e9272d583462c5f501d7470566c3ad1f3f9cc15b
Amol Bhople <amolbhople@gmail.com>
2021-03-28 00:53:49 +0530

    first commit

---
+++ src/test/java/ab/techstack/spring/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ab.techstack.spring.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
